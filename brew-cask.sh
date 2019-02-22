691da1833eb502fe228639f05cfb4d2a47f86b44
Edson Junior <soneddjr@gmail.com>
2019-02-22 14:02:54 +0100

    disable virtualbox, add java8

--- brew-cask.sh
+++ brew-cask.sh
@@ -8,6 +8,7 @@
 
 # brew install caskroom/cask/brew-cask
 # brew tap caskroom/versions
+brew tap homebrew/cask-versions
 
 # daily
 brew cask install dropbox
@@ -15,7 +16,7 @@ brew cask install enpass
 brew cask install docker 
 brew cask install docker-toolbox
 brew cask install skype
-brew cask install virtualbox
+# brew cask install virtualbox
 brew cask install the-unarchiver
 brew cask install 1password
 brew cask install kap
@@ -24,6 +25,7 @@ brew cask install kap
 brew cask install iterm2
 brew cask install visual-studio-code
 brew cask install sourcetree
+brew cask install java8
 
 # browsers
 brew cask install google-chrome
