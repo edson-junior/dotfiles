c4acfad87e26822e501eaa479f1bb538015d40db
Edson Junior <soneddjr@gmail.com>
2020-05-26 10:27:24 +0200

    hyper-solarized-dark fucks up git diff's green

--- .hyper.js
+++ .hyper.js
@@ -159,8 +159,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  // plugins: ["hyper-pane", "hyper-search", "hyper-snazzy"],
-  plugins: ["hyper-pane", "hyper-search", "hyper-solarized-dark"],
+  plugins: ["hyper-pane", "hyper-search", "hyper-snazzy"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
