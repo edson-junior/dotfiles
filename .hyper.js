71451b9ecc9b1d45eb246dddc0a111f202bf58f5
Edson Junior <soneddjr@gmail.com>
2020-05-26 09:39:05 +0200

    try out solarized dark theme

--- .hyper.js
+++ .hyper.js
@@ -159,7 +159,8 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-pane", "hyper-search", "hyper-snazzy"],
+  // plugins: ["hyper-pane", "hyper-search", "hyper-snazzy"],
+  plugins: ["hyper-pane", "hyper-search", "hyper-solarized-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
